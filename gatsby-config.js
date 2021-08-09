f88cc8a17f399ea9e0515419dfc229fe75854b01
Szymon Mazurczak <biuro@tenbajt.pl>
2021-08-09 07:32:16 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "tenbajt.pl",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
